c7d311eb51bbba9d2477fad0c0c67949dd9310b3
Anand Ranjan <44177280+Anand-ark@users.noreply.github.com>
2021-07-13 08:35:30 +0530

    Update and rename Simplify Path to Simplify Path.cpp

--- Stack/Simplify Path.cpp
+++ Stack/Simplify Path.cpp
@@ -1,4 +1,4 @@
-Given a string path, which is an absolute path (starting with a slash '/') to a file or directory in a Unix-style file system, 
+/*Given a string path, which is an absolute path (starting with a slash '/') to a file or directory in a Unix-style file system, 
 convert it to the simplified canonical path.
 
 In a Unix-style file system, a period '.' refers to the current directory, a double period '..' refers to the directory up a level, 
@@ -11,13 +11,13 @@ The path starts with a single slash '/'.
 Any two directories are separated by a single slash '/'.
 The path does not end with a trailing '/'.
 The path only contains the directories on the path from the root directory to the target file or directory (i.e., no period '.' or double period '..')
-Return the simplified canonical path.
+Return the simplified canonical path.*/
 
-SOLUTION 1: 
+/*SOLUTION 1: 
 -----------
 By looking at examples we can see that the above simplification process just behaves like a stack. Whenever we encounter any file’s name, 
 we simply push it into the stack. when we come across ” . ” we do nothing. When we find “..” in our path, we simply pop the topmost element 
-as we have to jump back to parent’s directory. When we see multiple “////” we just ignore them as they are equivalent to one single “/”.
+as we have to jump back to parent’s directory. When we see multiple “////” we just ignore them as they are equivalent to one single “/”.*/
 
 class Solution {
 public:
@@ -56,11 +56,13 @@ public:
         return ans;
     }
 };
+/*
 ------------
 SOLUTION 2:
 ------------
 In Solution 1, the directories so formed, are first pushed into the stack and then the stack is reversed to form the canonical path.
-The only optimization here is to reduce the number of stack operations and this can be done by using vectors in place of a stack.
+The only optimization here is to reduce the number of stack operations and this can be done by using vectors in place of a stack.*/
+
 class Solution {
 public:
     string simplifyPath(string path) 
